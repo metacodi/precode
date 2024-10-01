43b237a5133cb5af50d934512751974459a74779
Jordi Giral <jgiral@metacodi.com>
2024-10-01 16:20:55 +0200

    Update do.bat

--- do.bat
+++ do.bat
@@ -3,6 +3,10 @@ if [%1]==[pub] (
   npx ts-node publish/publish.ts %*
 )
 
+if [%1]==[metacodi] (
+  npx ts-node publish/upgrade-metacodi-dependencies.ts %*
+)
+
 if [%1]==[docs] (
   typedoc --out docs src/code --readme README.md
 )
